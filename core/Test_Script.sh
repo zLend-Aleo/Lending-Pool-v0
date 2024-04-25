59a511007091fe0fffed026d1519a7399215af9d
Zk <zklim95@hotmail.com>
2024-04-26 03:10:43 +0800

    feat: compound interest
    err: if statement will still execute even not satisfy, need workaround

--- core/Test_Script.sh
+++ core/Test_Script.sh
@@ -22,27 +22,27 @@ leo run print_address zlend_v0_0_0.aleo
 # Test deposit_credits_private
 echo -e "\n>>> Test deposit_credits_private <<<\n"
 echo -e "credits_record = ${credits_record}\n"
-leo run deposit_credits_private "{  owner: aleo1wsend9sju5qnp2n8hcwug5wkn99dhyaa9z0ghy62vecqs7mvmurqmavkz0.private,  microcredits: 100000000u64.private,  _nonce: 3912014721620745568731209339925199250584097142860810246990193295075541680748group.public}" ${amount}
+leo run deposit_credits_private "{  owner: ${owner}.private,  microcredits: ${amount}.private,  _nonce: 3912014721620745568731209339925199250584097142860810246990193295075541680748group.public}" ${amount}
 
 # Test deposit_stable_private
 echo -e "\n>>> Test deposit_stable_private <<<\n"
-leo run deposit_stable_private "{  owner: aleo1wsend9sju5qnp2n8hcwug5wkn99dhyaa9z0ghy62vecqs7mvmurqmavkz0.private,  amount: 100000000u64.private,  _nonce: 3912014721620745568731209339925199250584097142860810246990193295075541680748group.public}" ${amount}
+leo run deposit_stable_private "{  owner: ${owner}.private,  amount: ${amount}.private,  _nonce: 3912014721620745568731209339925199250584097142860810246990193295075541680748group.public}" ${amount}
 
 # Test withdraw_credits_private
 echo -e "\n>>> Test withdraw_credits_private <<<\n"
-leo run withdraw_credits_private "{  owner: aleo1wsend9sju5qnp2n8hcwug5wkn99dhyaa9z0ghy62vecqs7mvmurqmavkz0.private,  amount: 10000000u64.private,  _nonce: 3912014721620745568731209339925199250584097142860810246990193295075541680748group.public}" ${amount}
+leo run withdraw_credits_private "{  owner: ${owner}.private,  amount: ${amount}.private,  _nonce: 3912014721620745568731209339925199250584097142860810246990193295075541680748group.public}" ${amount}
 
 # Test withdraw_stable_private
 echo -e "\n>>> Test withdraw_stable_private <<<\n"
-leo run withdraw_stable_private "{  owner: aleo1wsend9sju5qnp2n8hcwug5wkn99dhyaa9z0ghy62vecqs7mvmurqmavkz0.private,  amount: 10000000u64.private,  _nonce: 3912014721620745568731209339925199250584097142860810246990193295075541680748group.public}" ${amount}
+leo run withdraw_stable_private "{  owner: ${owner}.private,  amount: ${amount}.private,  _nonce: 3912014721620745568731209339925199250584097142860810246990193295075541680748group.public}" ${amount}
 
 # Test add_collateral_credits
 echo -e "\n>>> Test add_collateral_credits <<<\n"
-leo run add_collateral_credits "{  owner: aleo1wsend9sju5qnp2n8hcwug5wkn99dhyaa9z0ghy62vecqs7mvmurqmavkz0.private,  amount: 10000000u64.private,  _nonce: 3912014721620745568731209339925199250584097142860810246990193295075541680748group.public}" ${amount}
+leo run add_collateral_credits "{  owner: ${owner}.private,  amount: ${amount}.private,  _nonce: 3912014721620745568731209339925199250584097142860810246990193295075541680748group.public}" ${amount}
 
 # Test add_collateral_stable
 echo -e "\n>>> Test add_collateral_stable <<<\n"
-leo run add_collateral_stable "{  owner: aleo1wsend9sju5qnp2n8hcwug5wkn99dhyaa9z0ghy62vecqs7mvmurqmavkz0.private,  amount: 10000000u64.private,  _nonce: 3912014721620745568731209339925199250584097142860810246990193295075541680748group.public}" ${amount}
+leo run add_collateral_stable "{  owner: ${owner}.private,  amount: ${amount}.private,  _nonce: 3912014721620745568731209339925199250584097142860810246990193295075541680748group.public}" ${amount}
 
 # Test borrow_credits
 echo -e "\n>>> Test borrow_credits <<<\n"
@@ -54,8 +54,8 @@ leo run borrow_stable ${amount}
 
 # Test repay_credits
 echo -e "\n>>> Test repay_credits <<<\n"
-leo run repay_credits "{  owner: aleo1wsend9sju5qnp2n8hcwug5wkn99dhyaa9z0ghy62vecqs7mvmurqmavkz0.private,  microcredits: 10000000u64.private,  _nonce: 3912014721620745568731209339925199250584097142860810246990193295075541680748group.public}" ${amount}
+leo run repay_credits "{  owner: ${owner}.private,  microcredits: ${amount}.private,  _nonce: 3912014721620745568731209339925199250584097142860810246990193295075541680748group.public}" ${amount}
 
 # Test repay_stable
 echo -e "\n>>> Test repay_stable <<<\n"
-leo run repay_stable "{  owner: aleo1wsend9sju5qnp2n8hcwug5wkn99dhyaa9z0ghy62vecqs7mvmurqmavkz0.private,  amount: 10000000u64.private,  _nonce: 3912014721620745568731209339925199250584097142860810246990193295075541680748group.public}" ${amount}
+leo run repay_stable "{  owner: ${owner}.private,  amount: ${amount}.private,  _nonce: 3912014721620745568731209339925199250584097142860810246990193295075541680748group.public}" ${amount}
